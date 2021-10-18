0a03fefaa413f74a0f4c5c04f3bd50e275c40fda
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-10-18 11:10:39 +0530

    Update extraplug.py

--- firebot/modules/extraplug.py
+++ firebot/modules/extraplug.py
@@ -10,24 +10,26 @@ import sys
 import asyncio
 import traceback
 import os
-from firebot.utils import edit_or_reply, fire_on_cmd, sudo_cmd
+import firebot.utils
+from firebot.events import register
 
-@fire.on(fire_on_cmd(pattern="extdl$", outgoing=True))
+
+@register(pattern="^.extdl", outgoing=True)
 async def install(event):
     if event.fwd_from:
         return
     chat = Var.PLUGIN_CHANNEL
-    documentss = await fire.get_messages(chat, None , filter=InputMessagesFilterDocument)
+    documentss = await borg.get_messages(chat, None , filter=InputMessagesFilterDocument)
     total = int(documentss.total)
     total_doxx = range(0, total)
     await event.delete()
     for ixo in total_doxx:
         mxo = documentss[ixo].id
-        downloaded_file_name = await event.client.download_media(await fire.get_messages(chat, ids=mxo), "firebot/modules/")
+        downloaded_file_name = await event.client.download_media(await borg.get_messages(chat, ids=mxo), "firebot/plugins/")
         if "(" not in downloaded_file_name:
             path1 = Path(downloaded_file_name)
             shortname = path1.stem
             load_module(shortname.replace(".py", ""))
-            await fire.send_message(event.chat_id, "Installed Plugin {} successfully.".format(os.path.basename(downloaded_file_name)))
+            await borg.send_message(event.chat_id, "Installed Plugin `{}` successfully.".format(os.path.basename(downloaded_file_name)))
         else:
-            await fire.send_message(event.chat_id, "Plugin {} has been pre-installed and cannot be installed.".format(os.path.basename(downloaded_file_name)))
+            await borg.send_message(event.chat_id, "Plugin `{}` has been pre-installed and cannot be installed.".format(os.path.basename(downloaded_file_name)))
